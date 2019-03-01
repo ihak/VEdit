b10c2282d6c485dabbfceb53d7c6854c3f25467d
Hassan Ahmed <hassan.ahmed@arpatech.com>
2019-03-01 17:49:50 +0500

    Initial commit

---
+++ VEdit/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  VEdit
+//
+//  Created by Hassan Ahmed on 01/03/2019.
+//  Copyright © 2019 Hassan Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let _ = VLCMediaPlayer()
+        
+    }
+
+
+}
+
